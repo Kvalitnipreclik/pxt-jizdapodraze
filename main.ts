4f71bd9f14420c7a391da6e1c5bee109088042e6
Kvalitnipreclik <115696189+Kvalitnipreclik@users.noreply.github.com>
2023-05-30 12:17:56 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
 
-let whiteLine:number
+let whiteLine = 1
 
 
 Sensors.SetLightLevel()
 
-
-Sensors.OnLightDrop(function() {
-    
-})
-
+let zamek = false
+//kolo 67mm
 
 const pinF = DigitalPin.P15
 const pinL = DigitalPin.P14
@@ -20,13 +17,43 @@ pins.setPull(pinR, PinPullMode.PullNone)
 
 
 basic.forever(function () {
+if (zamek){
 
-let c = (whiteLine ^ pins.digitalReadPin(pinF)) ==0 ? false : true
+    let Foward = (whiteLine ^ pins.digitalReadPin(pinF)) == 0 ? false : true
+    let Left = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true
+    let Right = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true
+
+    if (!Foward) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 100)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 100)
+    }
+    if (!Right){
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 70)
+    }
+    if (!Left) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 200)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 70)
+    }
+
+
+}
 
 
 
+    basic.pause(50)
+})
 
+input.onButtonPressed(Button.AB, function() {
+    PCAmotor.MotorStopAll()
+    
+})
 
+input.onButtonPressed(Button.A, function () {
+    if(zamek){
+        zamek = false
+    }else{
+        zamek = true
+    }
 
-    basic.pause(200)
 })
