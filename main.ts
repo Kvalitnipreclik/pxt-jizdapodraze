8cd48739bd0c626e8457e24a9298a595dd67939d
Kvalitnipreclik <115696189+Kvalitnipreclik@users.noreply.github.com>
2023-05-31 13:32:36 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
 
 let whiteLine = 1
+let historie = {
+            Foward: false,
+            Left: false,
+        Right: false 
+}
 
 
 Sensors.SetLightLevel()
-
+const data:Array<object> = []
+let objekt = {
+    Foward: false,
+     Left: false,
+      Right: false,
+ }
 let zamek = false
 //kolo 67mm
 
@@ -16,32 +26,51 @@ pins.setPull(pinL, PinPullMode.PullNone)
 pins.setPull(pinR, PinPullMode.PullNone)
 
 
-basic.forever(function () {
-if (zamek){
+function motory (){
 
-    let Foward = (whiteLine ^ pins.digitalReadPin(pinF)) == 0 ? false : true
-    let Left = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true
-    let Right = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true
 
-    if (!Foward) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, 100)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, 100)
-    }
-    if (!Right){
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, 70)
-    }
-    if (!Left) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, 200)
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, 70)
-    }
 
 
 }
 
 
 
-    basic.pause(50)
+basic.forever(function () {
+if (zamek){
+
+    let Foward = (whiteLine ^ pins.digitalReadPin(pinF)) == 0 ? false : true
+    let Left = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true
+    let Right = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true
+    objekt.Foward = Foward
+    objekt.Left = Left
+    objekt.Right = Right
+    
+    historie = data.pop()
+    if (!Foward) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 100)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 100)
+    } 
+    
+    if (!Right){
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 70)
+    }else if (Right && historie.Foward)
+    if (!Left) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 200)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 70)
+    }
+
+    data.push(objekt)
+
+ if( data.length > 10){
+     data.shift()
+ }
+     basic.pause(50)
+}
+
+
+
+  
 })
 
 input.onButtonPressed(Button.AB, function() {
@@ -57,3 +86,6 @@ input.onButtonPressed(Button.A, function () {
     }
 
 })
+
+
+
