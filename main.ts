c38be69a977925589dc2bb4081078ba40f3bb587
Kvalitnipreclik <115696189+Kvalitnipreclik@users.noreply.github.com>
2023-06-12 22:01:28 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 let whiteLine = 1
-
+let sparovan:number
 radio.setFrequencyBand(0)
 radio.setGroup(1)
 radio.setTransmitPower(7)
@@ -13,34 +13,53 @@ const data: Array<any> = [
 
     }
 ]
+input.onButtonPressed(Button.B, function () {
+    bezCary()
+})
+radio.onReceivedNumber(function (receivedNumber: number) {
 
-radio.onReceivedNumber(function(receivedNumber: number) {
-    smerjizdy = receivedNumber
+    if (receivedNumber === 24){
+        sparovan = RadioPacketProperty.SerialNumber
 
-    basic.showLeds(`
+        basic.showLeds(`
+    # # # # #
+    # # . # #
+    # . # . #
+    # . . . #
+    # # # # #
+    `)
+    } else if (RadioPacketProperty.SerialNumber === sparovan){
+        smerjizdy = receivedNumber
+
+
+        basic.showLeds(`
     . . . . .
     . . . # .
     # . # . .
     . # . . .
     . . . . .
     `)
+    }
+
+
+ 
 })
 
-  
-let stranaOtace = 2
+
+
 let spatnyMotor = 215 / 255
-let smerjizdy:number = 1
+let smerjizdy: number = 1
 let zamek = false
 //kolo 67mm
 
-let speeindwex = 150
+let speeindwex = 160
 let rovne: number
 let zatacky: number
-function motory(rovne:number, zatacky:number ) {
-    
-        let motorM1 = rovne + zatacky
-    let motorM4 = rovne - zatacky
-    
+function motory(rovne: number, zatacky: number) {
+
+    let motorM1 = rovne - zatacky
+    let motorM4 = (rovne + zatacky)
+
 
 
 
@@ -77,35 +96,34 @@ basic.forever(function () {
         let Left = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true
         let Right = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true
 
-  
-        objekt.foward = Foward
-        objekt.left = Left
-        objekt.right = Right
+        let ultrasonic = ping(DigitalPin.P2, DigitalPin.P1)
+     
+
+
 //modus
-let fowardModus = 0
-let rightModus = 0
-let leftModus = 0
+        let fowardModus = 0
+        let rightModus = 0
+        let leftModus = 0
 
 
-for(let i = 0; i < data.length - 1; i++){
-    fowardModus = fowardModus + data[i].foward
-    leftModus = leftModus + data[i].left
-    rightModus = rightModus + data[i].right
+        for (let i = 0; i < data.length - 1; i++) {
+            fowardModus = fowardModus + data[i].foward
+            leftModus = leftModus + data[i].left
+            rightModus = rightModus + data[i].right
 
-    if (fowardModus > (data.length - 1) * 0.7) { fowardModus = 1 } else { fowardModus = 0}
-    if (leftModus > (data.length - 1) * 0.7) { leftModus = 1 } else { leftModus = 0 }
-    if (rightModus > (data.length - 1) * 0.7) { rightModus = 1 } else { rightModus = 0 }
-}
-
-
-
+            if (fowardModus > (data.length - 1) * 0.7) { fowardModus = 1 } else { fowardModus = 0 }
+            if (leftModus > (data.length - 1) * 0.7) { leftModus = 1 } else { leftModus = 0 }
+            if (rightModus > (data.length - 1) * 0.7) { rightModus = 1 } else { rightModus = 0 }
+        }
 
 
 
-
-//krizovatka
+        //krizovatka
         if (!Left && !Right && !Foward) {
-            krizovatkaX()
+            if (leftModus === 0   && rightModus === 0) {
+                krizovatkaX()
+            }
+
 
 
         } else if (!Left && !Foward) {
@@ -113,30 +131,45 @@ for(let i = 0; i < data.length - 1; i++){
         } else if (!Right && !Foward) {
 
         }
-        if (!Left && !Right && !Foward){
+        
+        if (ultrasonic < 30){
 
+            basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
         }
 
 
-//zatacky
+
         if (!Right) {
-            rovne = 1
-            zatacky = -0.2
+            rovne = 0
+            zatacky = -0.7
             motory(rovne, zatacky)
-        } else if (Right && data[data.length - 1].right === false) {
-            rovne = 1
-            zatacky = -0.8
+        } else if (Right && Foward === false) {
+            rovne = 0.6
+            zatacky = -0.4
             motory(rovne, zatacky)
         }
         if (!Left) {
-            rovne = 1
-            zatacky = 0.2
+            rovne = 0
+            zatacky = 0.7
             motory(rovne, zatacky)
-        } else if (Left && data[data.length - 1].left === false) {
-            rovne = 1
-            zatacky = 0.8
+        } else if (Left && Foward === false) {
+            rovne = 0.6
+            zatacky = 0.4
             motory(rovne, zatacky)
         }
+        if (!Foward) {
+            rovne = 1
+            zatacky = 0
+            motory(rovne, zatacky)
+        }
+
+
         if (Left && Right && Foward) {
             if (data[data.length - 1].left === false) {
                 rovne = 1.2
@@ -146,29 +179,28 @@ for(let i = 0; i < data.length - 1; i++){
                 rovne = 1.2
                 zatacky = 0.4
                 motory(rovne, zatacky)
-            } else if (data[data.length - 1].foward === false){
+            } else if (data[data.length - 1].foward === false) {
                 bezCary()
             }
-
         }
 
-//mánévry
+        //mánévry
 
 
 
         motory(rovne, zatacky)
-      
 
 
 
- 
+
+
 
         data.push(objekt)
 
         if (data.length > 20) {
             data.shift()
         }
-        basic.pause(50)
+        basic.pause(10)
     }
 
 
@@ -176,52 +208,82 @@ for(let i = 0; i < data.length - 1; i++){
 
 })
 
+//ultrazvuk
+function ping(trig: DigitalPin, echo: DigitalPin, maxCmDistance = 500): number {
+    pins.setPull(trig, PinPullMode.PullNone)
+    pins.digitalWritePin(trig, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(trig, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(trig, 0)
 
+
+    const a = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58)
+    let sosCoef = 1 / ((331 + 0.607 * (25 - 4)) / 10000 / 2)
+    return Math.round(a / sosCoef)
+
+}
+
+
+function objetPrekazku(){
+
+
+
+
+}
+
+
+
+
+
+
+
+let stranaOtace = 2
 //zatacka o 90 stupnu
-function zatackaPravehoUhlu(){
+function zatackaPravehoUhlu() {
     motory(0, stranaOtace)
-    basic.pause(500)
+    basic.pause(375)
 }
 //krizovatkaX
-function krizovatkaX (){
-    
-        basic.showIcon(IconNames.Heart)
-        switch (smerjizdy) {
-            case 1:
-                motory(1, 0)
-                basic.pause(200)
-                stranaOtace = 2
-                zatackaPravehoUhlu()
-                
-                motory(1, 0)
-                basic.pause(500)
-                break;
-            case 2:
-                motory(1, 0)
-                basic.pause(200)
-                break;
-            case 3:
-                motory(1, 0)
-                basic.pause(200)
-                stranaOtace = -2
-                zatackaPravehoUhlu()
+function krizovatkaX() {
 
-                motory(1, 0)
-                basic.pause(500)
-                break
-            default:
-            // code block
-        }
+    basic.showIcon(IconNames.Heart)
+    switch (smerjizdy) {
+        case 1:
+            motory(1, 0)
+            basic.pause(200)
+            stranaOtace = 2
+            zatackaPravehoUhlu()
+
+            motory(1, 0)
+            basic.pause(500)
+            break;
+        case 2:
+            motory(1, 0)
+            basic.pause(200)
+            break;
+        case 3:
+            motory(1, 0)
+            basic.pause(200)
+            stranaOtace = -2
+            zatackaPravehoUhlu()
+
+            motory(1, 0)
+            basic.pause(500)
+            break
+        default:
+        // code block
+    }
 
 
-    
+
 }
 //krizovatkaT
-function krizovatkaT(){
+function krizovatkaT() {
     switch (orientaceKrizovatkyT) {
         case 1:
-           motory(0, 1)
-           basic.pause(50)
+            motory(0, 1)
+            basic.pause(50)
             motory(1, 0)
             break;
         case 2:
@@ -249,61 +311,33 @@ input.onButtonPressed(Button.A, function () {
 
 })
 // konec cary nebo preruseni
-function bezCary(){
+function bezCary() {
+
+    basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . # . .
+    . . # . .
+    `)
     let Foward = (whiteLine ^ pins.digitalReadPin(pinF)) == 0 ? false : true
     let Left = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true
     let Right = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true
-    motory(1,0)
-    basic.pause(2000)
-    if (!Left || !Right || !Foward){
-        
-    }else {
+    motory(2, 0)
+    basic.pause(1000)
+    if (!Left || !Right || !Foward) {
+
+    } else {
         zatackaPravehoUhlu()
         zatackaPravehoUhlu()
-        motory(1, 0)
-        basic.pause(2000)
+        motory(2, 0)
+        basic.pause(1000)
     }
 }
 
 
-function prekazka(){
+function prekazka() {
 
 
-    
+
 }
-//chat gpt
-
-// interface ObjectValues {
-//     forward: 0 | 1;
-//     left: 0 | 1;
-//     right: 0 | 1;
-// }
-
-// function calculateMode(objectArray: Object[]): [number, number, number] {
-//     const countMap:any = {
-//         forward0: 0,
-//         forward1: 0,
-//         left0: 0,
-//         left1: 0,
-//         right0: 0,
-//         right1: 0,
-//     };
-
-//     for (const obj of objectArray) {
-//         countMap[`forward${obj.forward}`]++;
-//         countMap[`left${obj.left}`]++;
-//         countMap[`right${obj.right}`]++;
-//     }
-
-//     let maxCount = 0;
-//     let mode: [number, number, number] = [0, 0, 0];
-
-//     for (const key in countMap) {
-//         if (countMap.hasOwnProperty(key) && countMap[key] > maxCount) {
-//             maxCount = countMap[key];
-//             mode = key.split('').map((value) => parseInt(value, 10)) as [number, number, number];
-//         }
-//     }
-
-//     return mode;
-// }
